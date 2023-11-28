40aa856a28ddf3933ecdd25860c94389761f7e94
LasiSipsara <sipsaralasini@gmail.com>
2023-11-28 12:02:54 +0530

    added footer

---
+++ Footer.php
@@ -0,0 +1,24 @@
+
+<footer>
+<div class="footer-content">
+    <div class="footer-social-icons">
+        <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+        <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+        <a href="#"><i class="fa-brands fa-square-whatsapp"></i></a>
+        <a href="#"><i class="fa-solid fa-square-envelope"></i></a>
+    </div>
+    <div class="footer-nav">
+        
+         <a href="">Home</a>
+         <a href="">About Us</a>
+         <a href="">Contact Us</a>
+      
+    </div>
+    
+  
+</div>
+<div class="footer-copyright">
+    <p>&copy;2023 Enchanted Blooms</p>
+    </div>
+
+</footer>
