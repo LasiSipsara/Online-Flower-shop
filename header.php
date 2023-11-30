9d1683a785a04b8dffe384b9af580c91a330e718
LasiSipsara <sipsaralasini@gmail.com>
2023-11-30 18:29:40 +0530

    added header and header css

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+    <input type="checkbox" name="" id="toggler">
+    <label for="toggler" class="fas fa-bars"></label>
+    <div class="header-logo">
+        <img src="asset\logo.png" alt="site Logo">
+    </div>
+
+    <nav class="header-navbar">
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
+    </nav>
+
+    <div class="header-icons">
+        <a href=""><i class="fa-solid fa-heart"></i></a>
+        <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+        <a href=""><i class="fa-solid fa-user"></i></a>
+       
+    </div>
+
+</header>
