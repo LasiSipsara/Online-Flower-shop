f74c0c99bb86d7bfac5ec6db48abdec7d5bfd8d1
ashenrashmike2000 <105934940+ashenrashmike2000@users.noreply.github.com>
2023-12-03 00:22:04 +0530

    Add files via upload

---
+++ asset/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+    Id int PRIMARY KEY AUTO_INCREMENT,
+    Firstname varchar(200),
+    Lastname varchar(200),
+    Email varchar(200),
+    Age int,
+    Password varchar(200)
+);
